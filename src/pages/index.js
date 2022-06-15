b8fd3a48bfe971ebc2bb380badb0af3142352b94
Naima Hassan <naima.hassan434@gmail.com>
2022-06-15 17:49:58 +0100

    React Router - Setup

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Checkout,
+  Cart,
+  Error,
+  PrivateRoute,
+};
