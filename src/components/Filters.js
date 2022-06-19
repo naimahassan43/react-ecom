5b7a281ca2319067472bf903ab6eb590fe259e72
Naima Hassan <naima.hassan434@gmail.com>
2022-06-19 11:31:31 +0100

    Filters - Unique Values

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -20,6 +20,13 @@ const Filters = () => {
     updateFilters,
     clearFilters,
   } = useFilterContext();
+
+  const categories = getUniqueValues(all_products, "category");
+  const companies = getUniqueValues(all_products, "company");
+  const colors = getUniqueValues(all_products, "colors");
+
+  console.log(colors);
+
   return (
     <Wrapper>
       <div className="content">
